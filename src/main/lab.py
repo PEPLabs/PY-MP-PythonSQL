ee050716ead4b0c44a3cc98b223d379fffc7a78b
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-03-15 13:35:48 -0400

    removed solutions from main

--- src/main/lab.py
+++ src/main/lab.py
@@ -5,32 +5,24 @@ as well as creating a table, inserting data, and selecting that data.
 import sqlite3
 
 
-conn = sqlite3.connect(":memory:")
-cursor = conn.cursor()
+conn = "TODO: Create a database connection"
+cursor = "TODO: create a cursor with the connection"
 
 
 # TODO: Complete create_dogs_table() by creating a table in the database called "dogs".
 def create_dogs_table():
 
-    # Create a table called "dogs"
-    cursor.execute("CREATE TABLE IF NOT EXISTS dogs (name TEXT, breed TEXT, age INTEGER)")
+    """TODO"""
 
 
 # TODO: Complete insert_dog() by inserting a new dog (provided in the parameters) into the "dogs" table.
 def insert_dog(name, breed, age):
 
-    # insert a new dog into the "dogs" table
-    cursor.execute("INSERT INTO dogs (name, breed, age) VALUES (?, ?, ?)", (name, breed, age))
+    """TODO"""
 
 
 # TODO: Complete select_all_dogs() by selecting all rows from the "dogs" table *and returning them*.
 def select_all_dogs():
 
-    # select all rows from the "dogs" table
-    cursor.execute("SELECT * FROM dogs")
-
-    # fetch all rows
-    rows = cursor.fetchall()
-
     # return the rows
-    return rows  # return "TODO: return the rows"
+    return """TODO"""
